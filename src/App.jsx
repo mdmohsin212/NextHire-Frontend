ea955753bce939ed0974a6890b24d907709223bf
md mohsin <siam.mohsin2005@gmail.com>
2025-03-07 11:13:58 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,74 @@ import Nav from "./pages/Nav";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import JobDeatils from "./pages/Job_deatils";
-import JobPost from './pages/Job_post';
+import JobPost from "./pages/Job_post";
 import EmployeProfile from "./pages/Employ_profile";
 import Applicant from "./pages/Applicant";
 import SeekerProfile from "./pages/Seeker_profile";
 import ShowJob from "./pages/Job";
-import Profile from './pages/Profile';
+import Profile from "./pages/Profile";
 import ChangePassword from "./pages/Change_password";
-import RunningJob from './pages/Running_job';
-import ChoisenCandate from './pages/Choisen_candidate';
-import CompleteJob from './pages/Complete_job';
+import RunningJob from "./pages/Running_job";
+import ChoisenCandate from "./pages/Choisen_candidate";
+import CompleteJob from "./pages/Complete_job";
 import { JobProvider } from "./context/JobContext";
+import PrivateRoute from "./context/PrivateRoute";
 
 function App() {
   return (
-    <>
-      <JobProvider>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/jobs" element={<ShowJob />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/job_details/:id" element={<JobDeatils />} />
-          <Route path="/post_job" element={<JobPost />} />
-          <Route path="/my_jobs" element={<EmployeProfile />} />
-          <Route path="/applicant_list/:id" element={<Applicant />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/running_job" element={<RunningJob />} />
-          <Route path="/applied_jobs" element={<SeekerProfile />} />
-          <Route path="/choisen_candidate" element={<ChoisenCandate />} />
-          <Route path="/change_password" element={<ChangePassword />} />
-          <Route path="/complete_job" element={<CompleteJob />} />
-        </Routes>
-      </JobProvider>
-    </>
-  );  
+    <JobProvider>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/jobs" element={<ShowJob />} />
+        <Route path="/job_details/:id" element={<JobDeatils />} />
+
+        <Route
+          path="/post_job"
+          element={<PrivateRoute element={<JobPost />} />}
+        />
+        <Route
+          path="/my_jobs"
+          element={<PrivateRoute element={<EmployeProfile />} />}
+        />
+        <Route
+          path="/applicant_list/:id"
+          element={<PrivateRoute element={<Applicant />} />}
+        />
+        <Route
+          path="/profile"
+          element={<PrivateRoute element={<Profile />} />}
+        />
+        <Route
+          path="/running_job"
+          element={<PrivateRoute element={<RunningJob />} />}
+        />
+        <Route
+          path="/applied_jobs"
+          element={<PrivateRoute element={<SeekerProfile />} />}
+        />
+        <Route
+          path="/choisen_candidate"
+          element={<PrivateRoute element={<ChoisenCandate />} />}
+        />
+        <Route
+          path="/change_password"
+          element={<PrivateRoute element={<ChangePassword />} />}
+        />
+        <Route
+          path="/complete_job"
+          element={<PrivateRoute element={<CompleteJob />} />}
+        />
+      </Routes>
+    </JobProvider>
+  );
 }
 
 export default App;
---
+++ src/context/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element }) => {
+  const isAuthenticated = !!localStorage.getItem("access_token");
+   return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
